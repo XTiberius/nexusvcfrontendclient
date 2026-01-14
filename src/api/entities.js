04e55e7add1c0618daee15b84a7769e9313b01e7
David Rashid <Germanicus1x@pm.me>
2026-01-14 17:03:43 -0400

    Initial commit for UI

---
+++ src/api/entities.js
@@ -0,0 +1,19 @@
+import { base44 } from './base44Client';
+
+
+export const Company = base44.entities.Company;
+
+export const Deal = base44.entities.Deal;
+
+export const AccessRequest = base44.entities.AccessRequest;
+
+export const Investment = base44.entities.Investment;
+
+export const NDA = base44.entities.NDA;
+
+export const Entity = base44.entities.Entity;
+
+
+
+// auth sdk:
+export const User = base44.auth;
